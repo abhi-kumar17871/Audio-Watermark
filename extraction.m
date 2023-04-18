2e816c8f43c343d95adecc7da1aacbc9a3be58ff
Abhishek Kumar <98654075+abhi-kumar17871@users.noreply.github.com>
2023-04-18 17:05:28 +0530

    audio_watermark using fft

--- extraction.m
+++ extraction.m
@@ -5,31 +5,27 @@ F=fft(y,nfft);
 F = F(1:nfft/2);
 mx = abs(F);
 f = (0:nfft/2-1)*fs/nfft;
-t = (0:length(y)-1) / fs;
 figure;
 subplot(2,1,1);
 plot(f,mx);
 
-%subplot(2,1,3);
-%plot(f,f);
-
-i=512;
-while(i<1024)
-if (mx(i)<0.75)
-    mx(i)=0;
-end
-i=i+1;
+i=656;
+while(i<=1024)
+    if (mx(i)<0.3)
+        mx(i)=0;
+    end
+    i=i+16;
 end
 
-X = zeros(10,1);
-i=640;
-j=0;
-while(i<768)
-    if(mx(i)>0.75)
+X = zeros(1,24);
+i=656;
+j=1;
+while(i<=1024)
+    if(mx(i)>0.3)
         X(j)=1;
     end
     j=j+1;
-    i=i+13;
+    i=i+16;
 end
 subplot(2,1,2);
 plot(f,mx);
